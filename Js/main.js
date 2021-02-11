f3c930e22cbd2cdd2e2b6a22a7247d5c7d159966
Adrian Gołębiewski <adrian.golebiewsk@gmail.com>
2021-02-11 11:27:46 +0100

    ImgPostion

--- Js/main.js
+++ Js/main.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const nav = document.querySelector('.navbar')
 
     function addShadow() {
-        if (window.scrollY >= 100) {
+        if (window.scrollY >= -1) {
             nav.classList.add('shadow-bg')
         } else {
             nav.classList.remove('shadow-bg')
